4191eebde1d580f3b38911b862b7c03582ee59c4
Aryan Pandey <aryanpandey048@gmail.com>
2024-11-04 14:31:54 -0800

    Added support for cat-file command to read blob objects

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import zlib
 
 
 def main():
@@ -8,7 +9,7 @@ def main():
 
     # Uncomment this block to pass the first stage
     
-    command = sys.argv[1]
+    command = sys.argv[1]    
     if command == "init":
         os.mkdir(".git")
         os.mkdir(".git/objects")
@@ -16,9 +17,28 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
         print("Initialized git directory")
+    elif command == "cat-file":
+        option = sys.argv[2]
+        if option == "-p":
+            blob_sha = sys.argv[3]
+            with open(f".git/objects/{blob_sha[:2]}/{blob_sha[2:]}", 'rb') as compressed_file:
+                object_type, size, content = parse_blob(compressed_file)
+                print(content, end='')
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
+
+def parse_blob(compressed_file):
+    compressed_data = compressed_file.read()
+    decompressed_data = zlib.decompress(compressed_data)
+    object_type_end_index = decompressed_data.index(b' ')
+    size_end_index = decompressed_data.index(b'\x00')
+
+    object_type = decompressed_data[:object_type_end_index].decode('utf-8')
+    size = decompressed_data[object_type_end_index + 1: size_end_index].decode('utf-8')
+    content = decompressed_data[size_end_index + 1:].decode('utf-8')
+
+    return object_type, size, content
 
 if __name__ == "__main__":
     main()
