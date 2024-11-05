8980bd382ebf90b7769a756cb49b69bf979ab1ed
Aryan Pandey <aryanpandey048@gmail.com>
2024-11-04 16:52:51 -0800

    Added support for ls-tree command to read tree object type

--- app/main.py
+++ app/main.py
@@ -7,10 +7,9 @@ import hashlib
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!", file=sys.stderr)
-
-    # Uncomment this block to pass the first stage
     
     command = sys.argv[1]    
+    option = ''
     if command == "init":
         os.mkdir(".git")
         os.mkdir(".git/objects")
@@ -23,7 +22,8 @@ def main():
         if option == "-p":
             blob_sha = sys.argv[3]
             with open(f".git/objects/{blob_sha[:2]}/{blob_sha[2:]}", 'rb') as compressed_file:
-                object_type, size, content = parse_blob(compressed_file)
+                decompressed_data = zlib.decompress(compressed_file.read())
+                object_type, size, content = parse_blob(decompressed_data)
                 print(content, end='')
     elif command == "hash-object":
         option = sys.argv[2]
@@ -44,14 +44,23 @@ def main():
 
             # Rename the temporary file to the final destination
             os.rename('temp', f'{target_dir}/{blob_sha[2:]}')
-
+    elif command =="ls-tree":
+        option = sys.argv[2]
+        if option == "--name-only":
+            tree_sha = sys.argv[3]
+            with open(f".git/objects/{tree_sha[:2]}/{tree_sha[2:]}", 'rb') as compressed_file:
+                decompressed_data = zlib.decompress(compressed_file.read())
+                print(decompressed_data, file=sys.stderr)
+                object_type, size, content = parse_tree(decompressed_data)                
+                for c in content:
+                    print(c['name'])
+        else:
+            tree_sha = sys.argv[2]
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
 
-def parse_blob(compressed_file):
-    compressed_data = compressed_file.read()
-    decompressed_data = zlib.decompress(compressed_data)
+def parse_blob(decompressed_data):
     object_type_end_index = decompressed_data.index(b' ')
     size_end_index = decompressed_data.index(b'\x00')
 
@@ -70,6 +79,29 @@ def calculate_sha1(data):
 
     # Get the hexadecimal digest of the hash
     return sha1_hash.hexdigest()
+
+def parse_tree(decompressed_data):
+    object_type_end_index = decompressed_data.index(b' ')
+    size_end_index = decompressed_data.index(b'\x00')
+
+    object_type = decompressed_data[:object_type_end_index].decode('utf-8')
+    size = decompressed_data[object_type_end_index + 1: size_end_index].decode('utf-8')
+    content = []
+    i = size_end_index + 1
+    while i < int(size):
+        mode_end_index = decompressed_data.index(b' ', i)
+        name_end_index = decompressed_data.index(b'\x00', i)
+        sha_end_index = name_end_index + 21
+
+        object = {}
+        object['mode'] = decompressed_data[i:mode_end_index].decode('utf-8')
+        object['name'] = decompressed_data[mode_end_index + 1: name_end_index].decode('utf-8')        
+        object['sha'] = decompressed_data[name_end_index + 1: sha_end_index]
+
+        content.append(object)
+        i = name_end_index + 21
+
+    return object_type, size, content
 
 if __name__ == "__main__":
     main()
