f156e177c9847ab753e3091e831dd088d9d08ab2
Aryan Pandey <aryanpandey048@gmail.com>
2024-11-05 21:11:04 -0800

    Added support for write-tree command to write tree objects. The contents of the current directly are assumed to be staged

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import sys
 import os
 import zlib
 import hashlib
-
+import traceback
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -55,7 +55,10 @@ def main():
                 for c in content:
                     print(c['name'])
         else:
+            # TODO: implement ls-tree without the --name-only option
             tree_sha = sys.argv[2]
+    elif command == "write-tree":
+        print(write_tree(os.getcwd()))
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
@@ -102,6 +105,43 @@ def parse_tree(decompressed_data):
         i = name_end_index + 21
 
     return object_type, size, content
+
+def create_blob_entry(path, write=True):
+    with open(path, "rb") as f:
+        data = f.read()
+    header = f"blob {len(data)}\0".encode("utf-8")
+    store = header + data
+    sha = hashlib.sha1(store).hexdigest()
+    if write:
+        os.makedirs(f".git/objects/{sha[:2]}", exist_ok=True)
+        with open(f".git/objects/{sha[:2]}/{sha[2:]}", "wb") as f:
+            f.write(zlib.compress(store))
+    return sha
+
+def write_tree(path: str):
+    if os.path.isfile(path):
+        return create_blob_entry(path)
+    contents = sorted(
+        os.listdir(path),
+        key=lambda x: x if os.path.isfile(os.path.join(path, x)) else f"{x}/",
+    )
+    s = b""
+    for item in contents:
+        if item == ".git":
+            continue
+        full = os.path.join(path, item)
+        if os.path.isfile(full):
+            s += f"100644 {item}\0".encode()
+        else:
+            s += f"40000 {item}\0".encode()
+        sha1 = int.to_bytes(int(write_tree(full), base=16), length=20, byteorder="big")
+        s += sha1
+    s = f"tree {len(s)}\0".encode() + s
+    sha1 = hashlib.sha1(s).hexdigest()
+    os.makedirs(f".git/objects/{sha1[:2]}", exist_ok=True)
+    with open(f".git/objects/{sha1[:2]}/{sha1[2:]}", "wb") as f:
+        f.write(zlib.compress(s))
+    return sha1
 
 if __name__ == "__main__":
     main()
